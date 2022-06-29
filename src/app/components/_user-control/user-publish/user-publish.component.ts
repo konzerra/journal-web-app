169683dae2e013a8669cb86697ce8cd7f925ef66
konzerra <konzerra@gmail.com>
2022-06-29 16:32:56 +0600

    sync

--- src/app/components/_user-control/user-publish/user-publish.component.ts
+++ src/app/components/_user-control/user-publish/user-publish.component.ts
@@ -42,7 +42,7 @@ export class UserPublishComponent
 
   journalList = new Array<Journal>(
     {
-      id: 4,
+      id: 5,
       name: "Журнал Известия им. КГТУ",
       version: "2022",
       status: "Open",
