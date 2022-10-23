6bf51933be90165580f7f4edc0af1a0577ce6865
Konzerra <konzerra@gmail.com>
2022-10-23 17:46:58 +0600

    config server jobs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/journal-web-app'));
+app.use(express.static(path.join(__dirname,'/build')));
 
 app.get('/*', (req, res) =>
   res.sendFile('index.html', {root: 'dist/journal-web-app/'}),
