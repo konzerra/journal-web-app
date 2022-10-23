50568a3f5a6eaf30f8bb507728747601c0783369
Konzerra <konzerra@gmail.com>
2022-10-23 18:07:06 +0600

    config server location

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname,'/build')));
+app.use(express.static('./dist/academic-journal-kstu-client'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/journal-web-app/'}),
+  res.sendFile('index.html', {root: 'dist/academic-journal-kstu-client/'}),
 );
 
 // Start the app by listening on the default Heroku port
