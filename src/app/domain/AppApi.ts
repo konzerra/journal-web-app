bfad6cd43d41027321ba3e6722870c0fdaf80382
Konzerra <konzerra@gmail.com>
2022-10-24 21:31:53 +0600

    changed journal image

--- src/app/domain/AppApi.ts
+++ src/app/domain/AppApi.ts
@@ -1,8 +1,7 @@
 
 export class AppApi {
   public static server = {
-    // publicApi : "https://academic-journal-kstu.herokuapp.com/api/v1/public",
-    // protectedApi: "https://academic-journal-kstu.herokuapp.com/api/v1/protected",
+
     publicApi : "http://localhost:8080/api/v1/public",
     protectedApi: "http://localhost:8080/api/v1/protected",
 
