1a21cd53f897a9ee887729eb38a3914727e5b19d
Konzerra <konzerra@gmail.com>
2022-11-21 14:22:05 +0600

    updated http changes

--- src/app/domain/AppApi.ts
+++ src/app/domain/AppApi.ts
@@ -2,8 +2,8 @@
 export class AppApi {
   public static server = {
 
-    publicApi : "https://api.journal.kstu.kg/api/v1/public",
-    protectedApi: "https://api.journal.kstu.kg/api/v1/protected",
+    publicApi : "http://api.journal.kstu.kg/api/v1/public",
+    protectedApi: "http://api.journal.kstu.kg/api/v1/protected",
 
   }
 
