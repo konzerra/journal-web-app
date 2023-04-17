db7d158b2efa32b4ccf0d56485ca550d10468628
Konzerra <konzerra@gmail.com>
2023-04-17 17:03:53 +0600

    changed to api address

--- src/app/domain/AppApi.ts
+++ src/app/domain/AppApi.ts
@@ -2,8 +2,8 @@
 export class AppApi {
   public static server = {
 
-    publicApi : "http://api.journal.kstu.kg/api/v1/public",
-    protectedApi: "http://api.journal.kstu.kg/api/v1/protected",
+    publicApi : "https://www.api.journal.kstu.kg/api/v1/public",
+    protectedApi: "https://www.api.journal.kstu.kg/api/v1/protected",
 
   }
 
